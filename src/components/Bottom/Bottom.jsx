011f856698f26fe0a8faa036c4cf90d7ae2ef176
Костя <kostabik@mail.ru>
2025-03-17 23:16:02 +0300

    First

---
+++ src/components/Bottom/Bottom.jsx
@@ -0,0 +1,9 @@
+import styles from './Bottom.module.css';
+
+export const Bottom = () => {
+    return (
+        <div className={styles.bottom}>
+            2025 © Все права и планета защищены. МАУ на страже вымирающих видов.
+        </div>
+    );
+};
